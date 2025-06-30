5870fb8e576f46ecdec05b19faf540613c20f775
jong-k <fully.charged07@gmail.com>
2025-06-30 22:45:42 +0900

    feat: 언어 토글 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import { Link } from "react-router";
+import { useTranslation, Trans } from "react-i18next";
 
 export default function App() {
+  const { t, i18n } = useTranslation();
+  console.log(t);
+  const count = 5;
+
+  const changeLanguage = () => {
+    const lng = i18n.language === "en" ? "ko" : "en";
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <div className="flex gap-4 flex-col">
       <Link to="/quotes">속담 리스트</Link>
       <Link to="/todo">할 일 리스트</Link>
+      <div>
+        <div>
+          <div>현재 언어: {i18n.language}</div>
+          <button onClick={changeLanguage}>언어 변경</button>
+        </div>
+        <p>{t("title", { name: "John" })}</p>
+        <p>{t("description.part1")}</p>
+        <p>{t("description.part2")}</p>
+        <Trans i18nKey="userMessagesUnread" count={count}>
+          You have {{ count }} unread message.
+        </Trans>
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import App from "./App.tsx";
 import Quotes from "./Quotes.tsx";
 import TodoListContainer from "./todo-list/TodoListContainer.tsx";
+import "./i18n";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
